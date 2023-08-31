ed0e1d86381b90bd20ce5a83c87ef0346486eefd
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-08-31 23:45:27 +0300

    refactor/ add async reducer

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: 'https://it-n00b-pel.github.io/module4/',
     resolve: {
         alias: {
             src: '/src',
